5f1b59abbf3ab14aa06dcf2ae58c0a5cb3f9c8c1
caldwell619 <caldwell8814@gmail.com>
2019-09-01 09:46:15 -0500

    showing the example config

--- src/config/config.example.py
+++ src/config/config.example.py
@@ -1,9 +1,20 @@
-api_key = 'YOUR_API_KEY_HERE'
+api_key = 'YOUR_GOOGLE_API_KEY_HERE'
 
 # both can be coordinates, or just 123 Sesame Street
-origin = 'YOUR_ORIGIN_HERE'.replace(' ', '+') # since there are no spaces in coordinates, this is fine
+# since there are no spaces in coordinates, this is fine
+origin = 'YOUR_ORIGIN_HERE'.replace(' ', '+')
 destination = 'YOUR_DESTINATION_HERE'.replace(' ', '+')
 
-# other options can be found here: 
+# other options can be found here:
 # https://developers.google.com/maps/documentation/directions/intro#VehicleType
-mode = 'driving' 
+mode = 'driving'
+
+# Screen output color
+color = 'yellow'
+
+minutes_between_commute_refresh = 1
+
+# 3 options for commutes are shown on the screen,
+# 1 is current, 2 is if you'd leave in x minutes, and 3 is if you left in y minutes
+option_2_minutes_offset = 20
+option_3_minutes_offset = 60
